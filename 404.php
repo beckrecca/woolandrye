dd0d12e72a08b986afbba602c47a22ff957dd432
beckrecca <rebecca.doris42@gmail.com>
2016-05-07 13:58:27 -0400

    404 doc

---
+++ 404.php
@@ -0,0 +1,17 @@
+ <!DOCTYPE html>
+<html>
+   <head>
+      <title>Whoops!</title>
+      <?php include("inc/htmlhead.php"); ?>
+   </head>
+   <body id="about">
+
+      <?php include("inc/logo.php"); ?>
+
+      <?php include("inc/nav.php"); ?>
+
+      <p>This page does not exist.</p>
+
+      <?php include("inc/footer.php") ?>
+   </body>
+</html>
