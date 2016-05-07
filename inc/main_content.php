e43238374968cf8660ce1497f1bcdd9ecfde10d4
beckrecca <rebecca.doris42@gmail.com>
2016-05-07 13:43:33 -0400

    html error fix

--- inc/main_content.php
+++ inc/main_content.php
@@ -1,3 +1,3 @@
 	  <div class="main">
-        <img src="img/clothing/pink-mitten.JPG" id="home-img" alt="One complete pink mitten with orange, pink, and green hearts, and the start of a second mitten with knitting needles attached and the pink ball of yarn adjacent." id="home-img" />
+        <img src="img/clothing/pink-mitten.JPG" alt="One complete pink mitten with orange, pink, and green hearts, and the start of a second mitten with knitting needles attached and the pink ball of yarn adjacent." id="home-img" />
       </div>
