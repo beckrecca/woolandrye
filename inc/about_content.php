bdf2db0119795f250e10b9839eba23d2954f6181
beckrecca <rebecca.doris42@gmail.com>
2016-05-08 21:45:06 -0400

    final about page edit

--- inc/about_content.php
+++ inc/about_content.php
@@ -3,20 +3,24 @@
           <h1>About</h1>
           <img src="img/clothing/pile.JPG" alt="A pile of folded knitted clothing, including green socks, an orange hat, purple mittens, a purple pom-pom hat, a grey sweater, a purple shawl, and a rainbow striped long scarf." class="left"/>
           <p>
-            Wool & Rye started in 2013 when Christina Doris realized she needed something to do 
-          with her hands. Hand-knitting is better for wearer and maker alike.
-          For the wearer, the handknit garment is more original and softer than any
-          commercial garment.
-          For the maker, the act is meditative and soothing.
-          When you purchase a sweater from Wool & Rye, you are purchasing a little piece of love.
+            In 2013, Christina Doris realized she needed something to do with her hands 
+            besides bake breads and cakes and Wool & Rye was born. She produces quality 
+            items using only the finest wools and wool blends sourced from independent 
+            dyers and local mills. Each item is hand knit with great care and attention to detail. 
+          </p>
+          Wool & Rye is dedicated to using premium quality fibres and yarns to produce hand-knit 
+          wear and hand-spun yarns. The fibres used to produce our hand-spun yarn are sourced from 
+          local farms and mills as often as possible. Wool & Rye both hand dyes their yarns and 
+          fibres and also supports independent hand dyers of the fibre community. 
+          <p>
           </p>
     		  <img src="img/yarn/wheel.JPG" alt="A wooden wheel for spinning wool with a foot pedal." class="right"/>
     		  <p>
-            Commercial yarn is produced by machines that stretch it much more tightly
-            than the hand-spinner would. The yarn loses its spring, its vivacity. 
-            While these machines spit out identical pieces of yarn, each skein of
-            handspun is unique. The texture of handspun yarn varies as much as people do.
-            
+            The hand-spun yarn from Wool & Rye is made on Christina's Ashford Kiwi 2 
+            spinning wheel and occasionally on a drop spindle. Our natural fleece is hand
+             washed and combed or carded by Christina before being transformed into yarn 
+             then knit-wear. Christina takes great pride in using raw materials from local, 
+             independent and small farms and fibre artists. 
           </p>
           
            </div>
