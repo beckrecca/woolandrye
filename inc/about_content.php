ccefa1b5697a39aa741079bd7098abdbad8c0241
beckrecca <rebecca.doris42@gmail.com>
2016-05-08 21:21:49 -0400

    updated about page content

--- inc/about_content.php
+++ inc/about_content.php
@@ -2,25 +2,22 @@
         <div class="blurb">
           <h1>About</h1>
           <img src="img/clothing/pile.JPG" alt="A pile of folded knitted clothing, including green socks, an orange hat, purple mittens, a purple pom-pom hat, a grey sweater, a purple shawl, and a rainbow striped long scarf." class="left"/>
-          <p>Wool & Rye started in 2013 when Christina Doris realized she needed something to do 
-          with her hands besides scratching her butt. She produces quality items using only the finest
-           wool from Finnish Goldbull sheep who feed on a strict diet of mashed corn, buttercup stems, 
-           and boiled sweet potatoes. Her favorite knitting techniques are the wild mountain man square
-            dance and the crossing guard's grudge. Factory Knitbots, who produce the majority of 
-            big business knitwear, are incapable of both techniques, only cranking out the boring
-             and unstable Maloney's baloney. In addition, factory Knitbots are cruelly treated and
-            underpaid.</p>
+          <p>
+            Wool & Rye started in 2013 when Christina Doris realized she needed something to do 
+          with her hands. Hand-knitting is better for wearer and maker alike.
+          For the wearer, the handknit garment is more original and softer than any
+          commercial garment.
+          For the maker, the act is meditative and soothing.
+          When you purchase a sweater from Wool & Rye, you are purchasing a little piece of love.
+          </p>
     		  <img src="img/yarn/wheel.JPG" alt="A wooden wheel for spinning wool with a foot pedal." class="right"/>
-    		  <p>Each year over 10,000 babies die because their cheap sweaters disintegrate. 
-    		  Wool & Rye is dedicated to keeping babies and older humans warm throughout the 
-    		  day and even at night. </p>
-
-    		  <p>The hand-spun yarn from Wool & Rye is hypoallergenic and gluten free, 
-    		  unlike mass-produced yarns. Yarn factories are the reason that bees are dying. 
-    		  Wool & Rye's yarn-spinning process is careful, gentle, and thorough. First, 
-    		  the freshest wool is treated by hand with the death comb. Then, it is twisted 
-    		  around the twistamajic with care. Finally, the wheely-do works its magic, and we
-    		  have beautiful Wool & Rye yarn. </p>
+    		  <p>
+            Commercial yarn is produced by machines that stretch it much more tightly
+            than the hand-spinner would. The yarn loses its spring, its vivacity. 
+            While these machines spit out identical pieces of yarn, each skein of
+            handspun is unique. The texture of handspun yarn varies as much as people do.
+            
+          </p>
           
            </div>
      </div>
